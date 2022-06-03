2681331511405c0b8966ab8aa0b87821ca5c904d
Luis Dudamel <ladusa@gmail.com>
2022-06-03 16:21:06 +0200

    Added load fav dogs test

---
+++ src/redux/thunks/dogsThunks.test.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { MockFavDogs } from "../../mocks/dogs";
+import { mockToken } from "../../mocks/userData";
+import { loadFavDogsActionCreator } from "../feature/dogsSlice";
+import { getFavDogsThunk } from "./dogsThunks";
+
+describe("Given a getFavDogsThunk", () => {
+  describe("When invoked with a valid token", () => {
+    test("Then it should return call the dispatch function", async () => {
+      const testToken = mockToken;
+      const dispatch = jest.fn();
+
+      axios.get = jest.fn().mockResolvedValue({
+        data: {
+          favdogs: MockFavDogs,
+        },
+      });
+
+      const expectedAction = loadFavDogsActionCreator(MockFavDogs);
+      const thunk = getFavDogsThunk(testToken);
+      await thunk(dispatch);
+
+      expect(dispatch).toHaveBeenCalledWith(expectedAction);
+    });
+  });
+});
