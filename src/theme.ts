4e687bd3bd6111e42c0ff7c114d42ea264844290
Luis Dudamel <ladusa@gmail.com>
2022-06-05 18:58:09 +0200

    Removed unused var

--- src/theme.ts
+++ src/theme.ts
@@ -1,18 +1,14 @@
 import { createTheme } from "@mui/material/styles";
 import { red } from "@mui/material/colors";
-import { bgcolor } from "@mui/system";
 
 const theme = createTheme({
   typography: {
     fontFamily: ["Outfit"].join(","),
   },
   components: {
-    // Name of the component
     MuiButton: {
       styleOverrides: {
-        // Name of the slot
         root: {
-          // Some CSS
           textTransform: "none",
           "&:hover": {
             backgroundColor: "#2a9d8f",
