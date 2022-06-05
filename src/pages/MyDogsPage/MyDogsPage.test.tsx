947b0fe376d180ded0459ce0580a174d9eee1bbf
Luis Dudamel <ladusa@gmail.com>
2022-06-05 15:19:34 +0200

    Test for mydogspage added

---
+++ src/pages/MyDogsPage/MyDogsPage.test.tsx
@@ -0,0 +1,50 @@
+import { ThemeProvider } from "@mui/material";
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import TestRenderer from "react-test-renderer";
+import { MockFavDogs } from "../../mocks/dogs";
+import store from "../../redux/store";
+import theme from "../../theme";
+import MyDogsPage from "./MyDogsPage";
+
+const mockDispatch = jest.fn();
+
+jest.mock("../../redux/hooks", () => ({
+  ...jest.requireActual("../../redux/hooks"),
+  useAppSelector: () => MockFavDogs,
+  useAppDispatch: () => mockDispatch,
+}));
+
+describe("Given a MyDogsPage component page", () => {
+  describe("When it's invoked", () => {
+    test("Then it should always match this snapshot", () => {
+      const testedMyDogsPage = TestRenderer.create(
+        <Provider store={store}>
+          <BrowserRouter>
+            <ThemeProvider theme={theme}>
+              <MyDogsPage />
+            </ThemeProvider>
+          </BrowserRouter>
+        </Provider>
+      );
+
+      expect(testedMyDogsPage).toMatchSnapshot();
+    });
+  });
+  describe("When its invoked", () => {
+    test("Then it should call the dispatch function with an username'", () => {
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <ThemeProvider theme={theme}>
+              <MyDogsPage />
+            </ThemeProvider>
+          </BrowserRouter>
+        </Provider>
+      );
+
+      expect(mockDispatch).toHaveBeenCalled();
+    });
+  });
+});
