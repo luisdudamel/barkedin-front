daecf33da480a80e00c35823ae6a36a15ebf730e
Luis Dudamel <ladusa@gmail.com>
2022-06-06 12:14:46 +0200

    Added test for deletefavdogthunk

--- src/redux/thunks/dogsThunks.test.ts
+++ src/redux/thunks/dogsThunks.test.ts
@@ -1,13 +1,16 @@
 import axios from "axios";
 import { mockFavDogs } from "../../mocks/dogs";
-import { loadFavDogsActionCreator } from "../feature/dogsSlice";
-import { getFavDogsThunk } from "./dogsThunks";
+import {
+  deleteFavDogActionCreator,
+  loadFavDogsActionCreator,
+} from "../feature/dogsSlice";
+import { deleteFavDogThunk, getFavDogsThunk } from "./dogsThunks";
 
 describe("Given a getFavDogsThunk", () => {
-  describe("When invoked with a valid token", () => {
-    test("Then it should return call the dispatch function", async () => {
+  describe("When invoked with a valid username", () => {
+    test("Then it should call the dispatch function with a list of dogs", async () => {
       const dispatch = jest.fn();
-
+      const mockUser = "luis1";
       axios.get = jest.fn().mockResolvedValue({
         data: {
           favdogs: mockFavDogs,
@@ -16,7 +19,29 @@ describe("Given a getFavDogsThunk", () => {
       });
 
       const expectedAction = loadFavDogsActionCreator(mockFavDogs);
-      const thunk = getFavDogsThunk("luis1");
+      const thunk = getFavDogsThunk(mockUser);
+      await thunk(dispatch);
+
+      expect(dispatch).toHaveBeenCalledWith(expectedAction);
+    });
+  });
+});
+
+describe("Given a deleteFavDogThunk", () => {
+  describe("When invoked with a valid id", () => {
+    test("Then it should call the dispatch function the valid id", async () => {
+      const dispatch = jest.fn();
+      const mockId = "1234";
+
+      axios.get = jest.fn().mockResolvedValue({
+        data: {
+          favdogs: mockFavDogs,
+        },
+        status: 200,
+      });
+
+      const expectedAction = deleteFavDogActionCreator(mockId);
+      const thunk = deleteFavDogThunk(mockId);
       await thunk(dispatch);
 
       expect(dispatch).toHaveBeenCalledWith(expectedAction);
