ccf3d5f0f479b2dd3d7997406e27b35eec9b7f34
Luis Dudamel <ladusa@gmail.com>
2022-06-08 13:48:06 +0200

    Added test for profilebar actions

--- src/components/ProfileBar/ProfileBar.test.tsx
+++ src/components/ProfileBar/ProfileBar.test.tsx
@@ -3,6 +3,13 @@ import userEvent from "@testing-library/user-event";
 import { BrowserRouter } from "react-router-dom";
 import { ProfileBar } from "./ProfileBar";
 
+const mockNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
+
 describe("Given a ProfileBar function", () => {
   describe("When its invoked", () => {
     test("Then it should render a button with the text 'Logout' and other with the test 'New dog'", () => {
@@ -23,6 +30,44 @@ describe("Given a ProfileBar function", () => {
 
       expect(expectedLogoutButton).toHaveTextContent(expectedLogoutText);
       expect(expectedNewDogButton).toHaveTextContent(expectedNewDogText);
+    });
+  });
+
+  describe("When its invoked and the user clicks on the Logout button", () => {
+    test("Then it should call navigate function to path '/login''", () => {
+      const expectedCalledPath = "/login";
+
+      render(
+        <BrowserRouter>
+          <ProfileBar />
+        </BrowserRouter>
+      );
+      const expectedLogoutButton = screen.getByRole("button", {
+        name: "Logout",
+      });
+
+      userEvent.click(expectedLogoutButton);
+
+      expect(mockNavigate).toHaveBeenCalledWith(expectedCalledPath);
+    });
+  });
+
+  describe("When its invoked and the user clicks on the 'New dog' button", () => {
+    test("Then it should call navigate function to path '/create''", () => {
+      const expectedCalledPath = "/create";
+
+      render(
+        <BrowserRouter>
+          <ProfileBar />
+        </BrowserRouter>
+      );
+      const expectedCreateButton = screen.getByRole("button", {
+        name: "New dog",
+      });
+
+      userEvent.click(expectedCreateButton);
+
+      expect(mockNavigate).toHaveBeenCalledWith(expectedCalledPath);
     });
   });
 });
