2240b96cf5b0fe894117a5d72123f585ae1ce6a0
Luis Dudamel <ladusa@gmail.com>
2022-06-12 16:21:06 +0200

    Added detail page by id

--- src/pages/DogDetailPage/DogDetailPage.tsx
+++ src/pages/DogDetailPage/DogDetailPage.tsx
@@ -2,15 +2,13 @@ import { useParams } from "react-router-dom";
 import { DogDetail } from "../../components/DogDetail/DogDetail";
 import { Header } from "../../components/Header/Header";
 import { NavBar } from "../../components/NavBar/Navbar";
-import { useAppSelector } from "../../redux/hooks";
+import { useAppDispatch } from "../../redux/hooks";
 import { IDog } from "../../interfaces/Dogs";
 import StyledDogDetailPage from "./StyledDogDetailPage";
+import { getDogByIdThunk } from "../../redux/thunks/dogsThunks";
+import { useEffect, useState } from "react";
 
 export const DogDetailPage = (): JSX.Element => {
-  const { id } = useParams();
-  const currentDogs = useAppSelector((state) => state.dogs);
-  const currentDogToShow = currentDogs.find((dog) => dog.id === id);
-
   const initialDogDetail: IDog = {
     name: "",
     age: 0,
@@ -23,12 +21,23 @@ export const DogDetailPage = (): JSX.Element => {
     toy: "",
     weight: "",
   };
+  const { id } = useParams();
+  const [actualDog, setActualDog] = useState(initialDogDetail);
+  const dispatch = useAppDispatch();
 
+  useEffect(() => {
+    (async () => {
+      try {
+        const { dog: dogToShow }: any = await dispatch(getDogByIdThunk(id));
+        setActualDog(dogToShow);
+      } catch {}
+    })();
+  }, [dispatch, id]);
   return (
     <>
       <StyledDogDetailPage>
-        <Header text={`${currentDogToShow?.name}'s profile`} />
-        <DogDetail dogToShow={currentDogToShow || initialDogDetail} />
+        <Header text={`${actualDog?.name}'s profile`} />
+        <DogDetail dogToShow={actualDog || initialDogDetail} />
       </StyledDogDetailPage>
       <NavBar />
     </>
