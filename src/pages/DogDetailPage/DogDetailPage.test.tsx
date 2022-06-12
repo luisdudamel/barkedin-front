37d3b5b222d4d5191f264a1af8ca55172ec4b6e6
Luis Dudamel <ladusa@gmail.com>
2022-06-12 18:33:09 +0200

    Added test for detail page

--- src/pages/DogDetailPage/DogDetailPage.test.tsx
+++ src/pages/DogDetailPage/DogDetailPage.test.tsx
@@ -1,15 +1,33 @@
 import { ThemeProvider } from "@mui/material";
+import { render, screen, waitFor } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import TestRenderer from "react-test-renderer";
+import { act } from "react-test-renderer";
+
 import store from "../../redux/store";
 import theme from "../../theme";
-import { DogDetailPage } from "./DogDetailPage";
 
-describe("Given a LoginPage component page", () => {
+import { DogDetailPage } from "./DogDetailPage";
+let mockId = "123";
+const mockNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+  useParams: () => ({
+    id: mockId,
+  }),
+}));
+
+jest.mock("../../redux/hooks", () => ({
+  ...jest.requireActual("../../redux/hooks"),
+  useAppSelector: () => "123",
+}));
+
+describe("Given a DogDetail component page", () => {
   describe("When it's invoked", () => {
-    test("Then it should always match this snapshot", () => {
-      const testedRegisterPage = TestRenderer.create(
+    test("Then it should render an image with the alt text 'personality'", async () => {
+      render(
         <Provider store={store}>
           <BrowserRouter>
             <ThemeProvider theme={theme}>
@@ -19,7 +37,9 @@ describe("Given a LoginPage component page", () => {
         </Provider>
       );
 
-      expect(testedRegisterPage).toMatchSnapshot();
+      const expectedImage = screen.getByAltText("personality");
+
+      expect(expectedImage).toBeInTheDocument();
     });
   });
 });
