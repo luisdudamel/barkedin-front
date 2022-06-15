4180864606ac672b19d4750d268c62c84d79f24f
Luis Dudamel <ladusa@gmail.com>
2022-06-15 20:52:48 +0200

    Covered lines missing on some tests

--- src/pages/DogDetailPage/DogDetailPage.test.tsx
+++ src/pages/DogDetailPage/DogDetailPage.test.tsx
@@ -1,7 +1,10 @@
 import { ThemeProvider } from "@mui/material";
 import { render, screen } from "@testing-library/react";
+import axios from "axios";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { act } from "react-test-renderer";
+import { mockFavDog } from "../../mocks/dogs";
 
 import store from "../../redux/store";
 import theme from "../../theme";
@@ -39,6 +42,37 @@ describe("Given a DogDetail component page", () => {
       const expectedImage = screen.getByAltText("personality");
 
       expect(expectedImage).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's invoked with a dog id", () => {
+    test("Then it should render a dog with an image with the text 'Fenix avatar''", async () => {
+      const expectedDogName = "Fenix avatar";
+
+      axios.get = jest.fn().mockResolvedValue({
+        data: {
+          dog: mockFavDog,
+        },
+        status: 200,
+      });
+      // eslint-disable-next-line testing-library/no-unnecessary-act
+      await act(async () => {
+        render(
+          <Provider store={store}>
+            <BrowserRouter>
+              <ThemeProvider theme={theme}>
+                <DogDetailPage />
+              </ThemeProvider>
+            </BrowserRouter>
+          </Provider>
+        );
+      });
+
+      const expectedText = screen.getByRole("img", {
+        name: expectedDogName,
+      });
+
+      expect(expectedText).toBeInTheDocument();
     });
   });
 });
